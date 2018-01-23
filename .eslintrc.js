b98227f00ffc62c8df325fb7936dfefa940b23dc
sreekar <sreekar339@gmail.com>
2018-01-23 22:22:46 +0530

    p1 passed

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": 0,
+        "no-undef": 0,
+        "no-unused-vars": 0
+    }
+};
---
+++ identity_test.js
@@ -0,0 +1,12 @@
+identity = require('./identity.js')
+
+describe('my identity function', () => {
+  test('takes and returns same number', () => {
+    expect(identity(2)).toBe(2)
+  })
+
+  test('takes and returns same object', () => {
+    const obj = { a: 1 }
+    expect(identity(obj)).toEqual(obj)
+  })
+})
